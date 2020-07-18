431c55df7c0267ae0678b396ee14e6cad9f451c2
Akashdeep-47 <61601588+Akashdeep-47@users.noreply.github.com>
2020-07-19 01:19:10 +0530

    Updating sg for mysql

--- main.tf
+++ main.tf
@@ -160,15 +160,6 @@ resource "aws_security_group" "sg-db" {
   	description = "Allow TLS inbound traffic"
   	vpc_id      = "${aws_vpc.ak-vpc.id}"
 
-
-  	ingress {
-    		description = "SSH"
-    		from_port   = 22
-    		to_port     = 22
-    		protocol    = "tcp"
-    		cidr_blocks = [ "0.0.0.0/0" ]
-  	}
-
   	ingress {
     		description = "MYSQL/Aurora"
     		from_port   = 3306
